470c08e73c215a28a60d0e4748e9d3ceaf51edfd
jay4yam <jay.ayamee@gmail.com>
2018-09-30 12:00:02 +0200

    Add SEO table, model, controller, repo

--- resources/views/back/blog/edit.blade.php
+++ resources/views/back/blog/edit.blade.php
@@ -82,7 +82,7 @@
                                                 <!-- auteur -->
                                                 <div class="form-group flex-column {!! $errors->has('user') ? 'has-error' : '' !!}">
                                                     {{ Form::label('title', 'AUTEUR :') }}
-                                                    {{ dd($article->user) }}
+                                                    {{ dd($article->user->profile()) }}
                                                     {{ Form::text('title', $article->user->profile->firstName, ['class' => 'form-control',  'disabled' => 'disabled']) }}
                                                     {!! $errors->first('title', '<small class="help-block">:message</small>') !!}
                                                 </div>
