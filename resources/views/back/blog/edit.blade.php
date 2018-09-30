1370e9ac83f5e924256196f4e174cf55a434441d
jay4yam <jay.ayamee@gmail.com>
2018-09-30 17:41:25 +0200

    test

--- resources/views/back/blog/edit.blade.php
+++ resources/views/back/blog/edit.blade.php
@@ -80,7 +80,7 @@
                                             <!-- 2eme col -->
                                             <div class="col-md-4 col-xs-12">
                                                 <!-- auteur -->
-                                                {{ $article->user->profile[0][firstName] }}
+                                                {{ $article->user->profile['0']['firstName'] }}
                                                 <div class="form-group flex-column {!! $errors->has('user') ? 'has-error' : '' !!}">
                                                     {{ Form::label('title', 'AUTEUR :') }}
                                                     {{ Form::text('title', $article->user->profile->firstName, ['class' => 'form-control',  'disabled' => 'disabled']) }}
