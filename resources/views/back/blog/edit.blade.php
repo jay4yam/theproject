3e817d385010a65f6903679bccb66cefb50a905a
jay4yam <jay.ayamee@gmail.com>
2018-09-30 19:14:28 +0200

    test

--- resources/views/back/blog/edit.blade.php
+++ resources/views/back/blog/edit.blade.php
@@ -82,7 +82,7 @@
                                                 <!-- auteur -->
                                                 <div class="form-group flex-column {!! $errors->has('user') ? 'has-error' : '' !!}">
                                                     {{ Form::label('title', 'AUTEUR :') }}
-                                                    {{ Form::text('title', $article->user->profile->firstName, ['class' => 'form-control',  'disabled' => 'disabled']) }}
+                                                    {{ Form::text('title', $article->user->profile->first()->firstName, ['class' => 'form-control',  'disabled' => 'disabled']) }}
                                                     {!! $errors->first('title', '<small class="help-block">:message</small>') !!}
                                                 </div>
 
