2ee8d601cdacb0f2bd9037a98f8ee915e3387019
jay4yam <jay.ayamee@gmail.com>
2019-01-27 10:58:46 +0100

    update voyage controller and repo

--- config/app.php
+++ config/app.php
@@ -217,6 +217,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class
     ],
 
 ];
