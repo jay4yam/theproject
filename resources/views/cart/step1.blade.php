86cc64bcaa0f1d64d4ce27d15e7a1917c4336e65
jay4yam <jay.ayamee@gmail.com>
2019-12-28 16:29:41 +0100

    update step1 html

--- resources/views/cart/step1.blade.php
+++ resources/views/cart/step1.blade.php
@@ -174,15 +174,15 @@
                     </div>
                     <div class="row">
                         <div class="col-md-4">
-                            <img src="{{ asset('images/100pour100-secure.png') }}" width="40%">
+                            <img src="{{ asset('images/100pour100-secure.png') }}" alt width="40%">
                             <p>{{ __('panier.100pc_secure') }}</p>
                         </div>
                         <div class="col-md-4">
-                            <img src="{{ asset('images/site-de-confiance.png') }}" width="40%">
+                            <img src="{{ asset('images/site-de-confiance.png') }}" alt width="40%">
                             <p>{{ __('panier.achat_garanti') }}</p>
                         </div>
                         <div class="col-md-4">
-                            <img src="{{ asset('images/site-prefere.png') }}" width="40%">
+                            <img src="{{ asset('images/site-prefere.png') }}" alt width="40%">
                             <p>{{ __('panier.100pc_satisfait') }}</p>
                         </div>
                     </div>
@@ -199,14 +199,14 @@
     <script>
         window.onload = function() {
             // Create a Stripe client.
-            var stripe = Stripe('pk_test_q1LEmC3fnWZrAl79CsxAhzY3');
+            let stripe = Stripe('pk_test_q1LEmC3fnWZrAl79CsxAhzY3');
 
             // Create an instance of Elements.
-            var elements = stripe.elements();
+            let elements = stripe.elements();
 
             // Custom styling can be passed to options when creating an Element.
             // (Note that this demo uses a wider set of styles than the guide below.)
-            var style = {
+            let style = {
                 base: {
                     color: '#32325d',
                     lineHeight: '18px',
@@ -224,14 +224,14 @@
             };
 
             // Create an instance of the card Element.
-            var card = elements.create('card', {style: style});
+            let card = elements.create('card', {style: style});
 
             // Add an instance of the card Element into the `card-element` <div>.
             card.mount('#card-element');
 
             // Handle real-time validation errors from the card Element.
             card.addEventListener('change', function(event) {
-                var displayError = document.getElementById('card-errors');
+                let displayError = document.getElementById('card-errors');
                 if (event.error) {
                     displayError.textContent = event.error.message;
                 } else {
@@ -240,7 +240,7 @@
             });
 
             // Handle form submission.
-            var form = document.getElementById('payment-form');
+            let form = document.getElementById('payment-form');
 
             form.addEventListener('submit', function(event) {
                 event.preventDefault();
@@ -248,7 +248,7 @@
                 stripe.createToken(card).then(function(result) {
                     if (result.error) {
                         // Inform the user if there was an error.
-                        var errorElement = document.getElementById('card-errors');
+                        let errorElement = document.getElementById('card-errors');
                         errorElement.textContent = result.error.message;
                     } else {
                         // Send the token to your server.
@@ -259,8 +259,8 @@
 
             function stripeTokenHandler(token) {
                 // Insert the token ID into the form so it gets submitted to the server
-                var form = document.getElementById('payment-form');
-                var hiddenInput = document.createElement('input');
+                let form = document.getElementById('payment-form');
+                let hiddenInput = document.createElement('input');
                 hiddenInput.setAttribute('type', 'hidden');
                 hiddenInput.setAttribute('name', 'stripeToken');
                 hiddenInput.setAttribute('value', token.id);
